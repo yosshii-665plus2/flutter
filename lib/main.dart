9db7bc7de267fedb654f435cf0833c73744f4936
yosshii <yosshii1415@gmail.com>
2026-07-18 21:23:52 +0900

    claude

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'dart:async';
 import 'dart:math';
-import 'dart:async'; // Timerを使うために必要
 import 'package:flutter/material.dart';
+const double boxSize = 80;
 
 void main() {
   runApp(const MyApp());
@@ -12,11 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: TapBox(), 
-        ),
-      ),
+      home: TapBox(),
     );
   }
 }
@@ -24,105 +21,146 @@ class MyApp extends StatelessWidget {
 class TapBox extends StatefulWidget {
   const TapBox({super.key});
 
-  // 「俺の本当の本体（State）は、下の『_TapBoxState』だよ」とFlutterに教えている
   @override
   State<TapBox> createState() => _TapBoxState();
 }
 
-class Boxes extends StatefulWidget{
+class Boxes extends StatefulWidget {
   final double top;
   final double left;
   final bool shin;
-  const Boxes({super.key,required this.top,required this.left,this.shin=false});
+  final VoidCallback onExpire; // タイムアウトしたときに親へ知らせる連絡係
 
+  const Boxes({
+    super.key,
+    required this.top,
+    required this.left,
+    required this.onExpire,
+    this.shin = false,
+  });
+
+  @override
   State<Boxes> createState() => _BoxesState();
-} 
+}
 
-class _BoxesState extends State<Boxes>
-{
+class _BoxesState extends State<Boxes> {
   Timer? _timer;
-  void startTimer() {
-    // 1秒ごとに {} の中身を実行するタイマーを起動！
-    _timer = Timer.periodic(const Duration(seconds: 10), (timer) {
-      setState(() {
-        if(widget.shin)counta=0;
-        boxlist.remove(this);
-      });
-    });
-  }
-  
+
   @override
   void initState() {
     super.initState();
-    // 画面が組み立てられた（誕生した）瞬間に、自動で関数を実行する！
-    startTimer(); 
+    // 10秒後に1回だけ発火（periodicではなくTimerでOK）
+    _timer = Timer(const Duration(seconds: 10), () {
+      widget.onExpire(); // 親(TapBox)に「時間切れになった」と連絡する
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel(); // タップされて先に消えた場合、発火しないように止める（超重要）
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Positioned( 
+    return Positioned(
       top: widget.top,
       left: widget.left,
-      child:Container(
-        width: 80, 
-        height: 80,
-        color: Colors.orange
-      )
+      child: Container(
+        width: boxSize,
+        height: boxSize,
+        color: Colors.orange,
+      ),
     );
   }
 }
 
-
-
 class _TapBoxState extends State<TapBox> {
-  List<Boxes> boxlist=[];
-  Random _random = Random();
-  int counta=0;
+  final List<Boxes> boxlist = [];
+  final Random _random = Random();
+  int counta = 0;
 
-  void how_boxes(Size gamenSize)
-  {
-    List<Boxes> _boxlist_tmp=[];
-    int _i=0;
-    double _random_top=0;
-    double _random_left=0;
-    while(true)
-    {
-      double boxsize=80;
-      _random_top=_random.nextDouble()*(gamenSize.height-boxsize);
-      _random_left=_random.nextDouble()*(gamenSize.width-boxsize);
-
-      if(_i>=counta){break;}
-      _boxlist_tmp.add(Boxes(top: _random_top, left: _random_left));
-      _i++;
-    }
-      if(counta==0){_boxlist_tmp.add(Boxes(top: gamenSize.height/2-40, left: gamenSize.width/2-40));}
-      else _boxlist_tmp.add(Boxes(top: _random_top, left: _random_left,shin: true));
-    setState(() {
-      counta++;
-      boxlist=_boxlist_tmp;  
+  @override
+  void initState() {
+    super.initState();
+    // 最初の1マスを、画面サイズが確定したあとのフレームで配置する
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      how_boxes(MediaQuery.of(context).size);
     });
   }
-  
-    @override
-    Widget build(BuildContext context) {
-      final Size screenSize = MediaQuery.of(context).size;
-      return Scaffold(
-        body: Stack(
-          children: [
-            for(var info in boxlist)
-            GestureDetector(
-              onTap: () {
-                info.shin==false
-                ?setState(() {boxlist.remove(info);})
-                :how_boxes(screenSize);
-              },
-              child:info
-              
-            )
-          ],
-        ),
-      ) ;
-      
 
-    }
+  // タップ or タイムアウトでマスを1個消す共通処理
+  void _removeBox(Boxes box) {
+    if (!mounted) return;
+    setState(() {
+      if (box.shin) counta = 0; // 本物を見逃したら最初からやり直し
+      boxlist.remove(box);
+    });
   }
+
+  // ランダムな位置のマスを1個作る（onExpireの中で自分自身を参照するのでlateを使う）
+  Boxes _makeBox(double top, double left, {bool shin = false}) {
+    late Boxes box;
+    box = Boxes(
+      top: top,
+      left: left,
+      shin: shin,
+      onExpire: () => _removeBox(box),
+    );
+    return box;
+  }
+
+  void how_boxes(Size gamenSize) {
+    final List<Boxes> tmp = [];
+    double randomTop = 0;
+    double randomLeft = 0;
+
+    for (int i = 0; i < counta; i++) {
+      randomTop = _random.nextDouble() * (gamenSize.height - boxSize);
+      randomLeft = _random.nextDouble() * (gamenSize.width - boxSize);
+      tmp.add(_makeBox(randomTop, randomLeft));
+    }
+
+    if (counta == 0) {
+      tmp.add(_makeBox(
+        gamenSize.height / 2 - boxSize / 2,
+        gamenSize.width / 2 - boxSize / 2,
+        shin: true,
+      ));
+    } else {
+      randomTop = _random.nextDouble() * (gamenSize.height - boxSize);
+      randomLeft = _random.nextDouble() * (gamenSize.width - boxSize);
+      tmp.add(_makeBox(randomTop, randomLeft, shin: true));
+    }
+
+    setState(() {
+      counta++;
+      boxlist
+        ..clear()
+        ..addAll(tmp);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Size screenSize = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        children: [
+          for (final info in boxlist)
+            GestureDetector(
+              key: ObjectKey(info), // リストの中身が入れ替わってもStateを正しく対応させる
+              onTap: () {
+                if (!info.shin) {
+                  _removeBox(info);
+                } else {
+                  how_boxes(screenSize);
+                }
+              },
+              child: info,
+            ),
+        ],
+      ),
+    );
+  }
+}
